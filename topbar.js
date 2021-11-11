57ae3cf16b4247079770f32bf9f253e2cc075a90
HYEONGDALKIM <guddak@kakao.com>
2021-11-11 15:41:48 +0900

    11.11

---
+++ topbar.js
@@ -0,0 +1,125 @@
+document.write(`
+<div style="height:5px; background-color: #326996"></div>
+<nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+  <div class="container">
+    <a class="navbar-brand" href="../index.html"><p style ="font-size:30px"><span class="text-primary">PB</span><span style="font-size:1.2em; color : rgb(5, 5, 5)";>FY</span></p></a> <!--PBFY-->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport" aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupport">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <div class="dropdown" style="margin-left:280px; ">
+            <button class="dropbtn"><strong>Chapter 1</strong></button>
+            <div class="dropdown-content">
+              <a href="./1-1.html">파이썬이란?</a>
+              <a href="./1-2.html">파이썬의 특징</a>
+              <a href="./1-3.html">파이썬으로 할 수 있는 것</a>
+              <a href="./1-4.html">파이썬 설치하기</a>
+              <a href="./1-5.html">파이썬 둘러보기</a>
+              <a href="./1-6.html">파이썬과 에디터</a>
+            </div>
+          </div>
+        </li>
+        <li class="nav-item">
+          <div class="dropdown" style="margin-left:20px; " >
+            <button class="dropbtn"><strong>Chapter 2</strong></button>
+            <div class="dropdown-content">
+              <a href="../2/2-1.html">숫자형</a>
+              <a href="../2/2-2.html">문자열 자료형</a>
+              <a href="../2/2-3.html">리스트 자료형</a>
+              <a href="../2/2-4.html">튜플 자료형</a>
+              <a href="../2/2-5.html">딕셔너리 자료형</a>
+              <a href="../2/2-6.html">집합 자료형</a>
+              <a href="../2/2-7.html">Bool 자료형</a>
+              <a href="../2/2-8.html">변수</a>
+              <a href="../2/2-9.html">2장 연습문제</a>
+            </div>
+          </div>
+        </li>
+        <li class="nav-item">
+          <div class="dropdown" style="margin-left:20px; ">
+            <button class="dropbtn"><strong>Chapter 3</strong></button>
+            <div class="dropdown-content">
+              <a href="../3/3-1.html">if문</a>
+              <a href="../3/3-2.html">while문</a>
+              <a href="../3/3-3.html">for문</a>
+              <a href="../3/3-4.html">3장 연습문제</a> 
+            </div>
+          </div>
+        </li>
+        <li class="nav-item">
+          <div class="dropdown" style="margin-left:20px; ">
+            <button class="dropbtn"><strong>Chapter 4</strong></button>
+            <div class="dropdown-content">
+              <a href="../4/4-1.html">함수</a>
+              <a href="../4/4-2.html">사용자 입출력</a>
+              <a href="../4/4-3.html">파일 읽고 쓰기</a>
+              <a href="../4/4-4.html">4장 연습문제</a>
+            </div>
+          </div>
+        </li>
+        <li class="nav-item">
+        <div class="dropdown" style="margin-left:20px; ">
+          <button class="dropbtn"><strong>Chapter 5</strong></button>
+          <div class="dropdown-content">
+            <a href="../5/5-1.html">클래스</a>
+            <a href="../5/5-2.html">모듈</a>
+            <a href="../5/5-3.html">패키지</a>
+            <a href="../5/5-4.html">예외처리</a>
+            <a href="../5/5-5.html">내장함수</a>
+            <a href="../5/5-6.html">라이브러리</a>
+            <a href="../5/5-7.html">5장 연습문제</a>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown" style="margin-left:20px; ">
+          <button class="dropbtn"><strong>Chapter 6</strong></button>
+          <div class="dropdown-content">
+            <a href="../6/6-1.html">내가 프로그램을?</a>
+            <a href="../6/6-2.html">3과 5의 배수합</a>
+            <a href="../6/6-3.html">게시판 페이징</a>
+            <a href="../6/6-4.html">간단한 메모장</a>
+            <a href="../6/6-5.html">탭 > 공백4개</a>
+            <a href="../6/6-6.html">하위 디렉터리 검색</a>
+            <a href="../6/6-7.html">파이보</a>
+            <a href="../6/6-8.html">코딩도장</a>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown" style="margin-left:20px; ">
+          <button class="dropbtn"><strong>Chapter 7</strong></button>
+          <div class="dropdown-content">
+            <a href="../7/7-1.html">정규표현식(1)</a>
+            <a href="../7/7-2.html">정규표현식(2)</a>
+            <a href="../7/7-3.html">정규표현식(3)</a>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown" style="margin-left:20px; ">
+          <button class="dropbtn"><strong>Chapter 8</strong></button>
+          <div class="dropdown-content">
+            <a href="../8/8-1.html">종합 문제</a>
+            <a href="../8/8-2.html">풀이</a>
+            <a href="../8/8-3.html">공부를 마치며</a>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item" style="margin-top:16px;">
+        <div style="margin-left:20px; ">
+          <a class="nav-link" href="../re_site.html"><p style = " font-size:1.0em;  "><b>참고사이트</b></p></a>
+        </div>
+      </li>
+      <li class="nav-item" style="margin-top:16px;">
+          <a class="nav-link" href="../LP.html"><p style = " font-size:1.0em;  "><b>학습플래닛</b></p></a>
+      </li>
+      </ul>
+    </div> 
+  </div> 
+</nav>
+<div style="height:4px; background-color: #326996"></div>
+
+`);
